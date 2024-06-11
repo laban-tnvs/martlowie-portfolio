271f78623cffa0a458c42ad5eb7dd1efbc0eb97c
martlowiesantiago <santiagomartlowie@gmail.com>
2024-06-11 21:53:45 +0800

    asd

--- Components/Footer.tsx
+++ Components/Footer.tsx
@@ -1,6 +1,6 @@
 import { DevicePhoneMobileIcon, EnvelopeIcon, MapIcon } from '@heroicons/react/20/solid'
 import React from 'react'
-
+//
 const Footer = () => {
   return (
     <div className="pt-[8rem] pb-[4rem] bg-[#02050a]">
